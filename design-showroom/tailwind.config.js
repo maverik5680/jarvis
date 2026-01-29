480c9b1b480e25d17d95141edef227c14484f993
Glen Dufour <maverik5680@gmail.com>
2026-01-29 16:51:12 -0500

    Install Tailwind CSS and add configuration

---
+++ design-showroom/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./**/*.html",
+    "./**/*.js",
+    "../ds/src/**/*.js",
+    "../ds/src/**/*.css",
+  ],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
