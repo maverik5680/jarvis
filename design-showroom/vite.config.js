91ef5aa7e166a8c324d46a9c5f341e79dd20b8af
Glen Dufour <maverik5680@gmail.com>
2026-01-29 12:13:14 -0500

    fix: updated root directory for build

--- design-showroom/vite.config.js
+++ design-showroom/vite.config.js
@@ -1,2 +1,13 @@
-import baseConfig from '../vite-config/base.config.js';
-export default baseConfig;
+// jarvis/design-showroom/vite.config.js
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // This allows Vite to see your CSS in the ../ds/ folder
+  server: {
+    fs: {
+      allow: ['..']
+    }
+  },
+  // This tells Vite where your shared images/assets are
+  publicDir: '../ds/public'
+})
